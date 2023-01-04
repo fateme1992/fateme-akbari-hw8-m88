1a9d38b250a021d7041a2c1539ad6ecc73dab653
Fateme Akbari <fateme.akbari71@gmail.com>
2023-01-04 23:00:21 +0330

    update 10/14

--- dom/1-5/main.js
+++ dom/1-5/main.js
@@ -1,40 +1,66 @@
-// let body = document.getElementById('body')
-// body.style.backgroundColor = '#c2cbc2'
-// body.style.height='100vh'
-// body.style.display='flex'
-
-// let div = document.createElement('div')
-// body.appendChild(div)
-// div.style.display='flex'
-// div.style.flexDirection='column'
-// div.style.alignItems='center'
-// div.style.margin='auto'
-// div.style.backgroundColor='white'
-// div.style.borderRadius='5px'
-// div.style.padding='1rem'
-
-// let divName=document.createElement('div')
-// div.appendChild(divName)
-// // divName.style.display='flex'
-// divName.style.marginBottom='16px'
-
-// let inputName = document.createElement('input')
-// divName.appendChild(inputName)
-// inputName.style.marginRight='5px'
-
-// let nullInput=document.createElement('p')
-// divName.appendChild(nullInput)
-// nullInput.innerHTML='الزامی'
+let userName = document.querySelector('.userName')
+let pass = document.querySelector('.pass')
+let confirm = document.querySelector('.confirm')
+let register = document.querySelector('button')
+let eName = document.querySelector('#eName')
+let ePass = document.querySelector('#ePass')
+let eConfirm = document.querySelector('#eConfirm')
+var regularExpression = /^[a-zA-Z0-9]{7,16}$/;
 
 
 
-// let lableName=document.createElement('label')
-// divName.appendChild(lableName)
-// lableName.innerHTML='نام کاربری'
+
+pass.addEventListener('keyup', function (e) {
+    let passValue = pass.value;
 
 
+    for (let i = 0; i < passValue.length; i++) {
+        pass.value = passValue.replace(passValue[i], '.')
+    }
 
-let inputPass = document.createElement('input')
-div.appendChild(inputPass)
 
+})
+
+confirm.addEventListener('keyup', function (e) {
+    let confirmValue = confirm.value;
+    for (let i = 0; i < confirmValue.length; i++) {
+        confirm.value = confirmValue.replace(confirmValue[i], '.')
+    }
+})
+
+register.onclick = function () {
+
+    if (pass.value.length < 8) {
+        ePass.innerHTML = "*رمز عبور باید حداقل شامل 8 کاراکتر و شامل حداقل یک حرف و یک عدد باشد"
+    }
+
+
+    if (userName.value == '' || pass.value == '' || confirm.value == '') {
+        eName.innerHTML = "*الزامی"
+        ePass.innerHTML = "*الزامی"
+        eConfirm.innerHTML = "*با رمز عبور مطابقت ندارد"
+
+    }
+    else {
+        eName.innerHTML = ""
+        ePass.innerHTML = ""
+        eConfirm.innerHTML = ""
+    }
+
+
+    if (pass.value !== confirm.value) {
+        eConfirm.innerHTML = "*با رمز عبور مطابقت ندارد"
+    }
+
+    // if (pass.value.length < 8) {
+    //     ePass.innerHTML = "*رمز عبور باید حداقل شامل 8 کاراکتر و شامل حداقل یک حرف و یک عدد باشد"
+    // }
+
+
+    if (regularExpression.test(pass.value)) {
+        alert("password should contain atleast one number and one special character");
+    }
+
+
+}
 
--- dom/1-6/main.js
+++ dom/1-6/main.js
@@ -1,9 +1,23 @@
-console.log('hello');
+var selectedRow = null
+let container = document.querySelector('.container')
+let main = document.querySelector('main')
 let table = document.getElementById('table')
 let thead = document.getElementById('thead')
-let tbody = document.createElement('tbody')
+let tbody = document.querySelector('tbody')
+let uid = document.querySelector('.uid')
+let firstname = document.querySelector('.firstname')
+let lastname = document.querySelector('.lastname')
+let city = document.querySelector('.city')
+let postalCode = document.querySelector('.postalCode')
+let phoneNumber = document.querySelector('.phoneNumber')
+let position = document.querySelector('.position')
+let submit = document.querySelector('.submit')
+let userList = document.querySelector('#user-list')
+let userForm = document.querySelector('#user-form')
+let edit = document.querySelector('.edit');
+let deleteBtn = document.querySelector('.delete')
 
-table.appendChild(tbody)
+
 let theadArray = Object.keys(userData[0])
 let trh = thead.insertRow();
 
@@ -11,12 +25,10 @@ let trh = thead.insertRow();
 for (let i = 0; i < theadArray.length; i++) {
     let th = trh.insertCell()
     th.appendChild(document.createTextNode(theadArray[i]));
-
     th.onclick = function () {
         sort(theadArray[i])
     }
 }
-
 
 
 //-------- CREATE TABLE BODY ---------//
@@ -31,37 +43,11 @@ function tableBodyGenerator(userData) {
             } else {
                 const td = trb.insertCell();
                 td.appendChild(document.createTextNode(Object.values(userData[i])[j]));
-                td.style.border = '1px solid black';
-                td.onclick = function () {
-                    let read = document.getElementById('read')
-                    read.innerHTML = ''
-                    for (let i = 0; i < theadArray.length; i++) {
-                        
-                        let p = document.createElement('p')
-                        read.appendChild(p)
-                       console.log( userData.find(theadArray[i]));
-                        p.innerHTML = `${theadArray[i]}:`
-                            // userData[]`
-
-
-
-                    }
-                }
-
-                //     let read = document.getElementById(read)
-                //     for(let i=0; i<theadArray;i++){
-                //         let p = document.createElement('p')
-                //         read.appendChild(p)
-                //         // p.innerHTML=`name: ${}`
-                //     }
-                // }
-
             }
         }
     }
 }
 tableBodyGenerator(userData)
-
 
 function sort(element) {
     userData.sort(dynamicSort(element));
@@ -80,5 +66,136 @@ function dynamicSort(ele) {
     }
 }
 
+function showAlert(message) {
+    let div = document.createElement('div')
+    div.className = "alert"
+    div.style.backgroundColor = "red"
+
+    div.appendChild(document.createTextNode((message)))
+
+    container.insertBefore(div, main)
+
+    setTimeout(() => document.querySelector('.alert').remove(), 3000)
+}
 
 
+//clear all data
+function clearFields() {
+    document.querySelector('.uid').value = ""
+    document.querySelector('.firstname').value = ""
+    document.querySelector('.lastname').value = ""
+    document.querySelector('.city').value = ""
+    document.querySelector('.postalCode').value = ""
+    document.querySelector('.phoneNumber').value = ""
+    document.querySelector('.position').value = ""
+
+}
+
+// ADD DATA
+submit.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+    if (firstname.value === "" || lastname.value === "" || uid.value === "") {
+        showAlert("please fill all fields", "danger")
+    }
+    else {
+        if (selectedRow === null) {
+
+
+            let row = document.createElement('tr')
+
+            row.innerHTML = `
+            <td>${uid.value}</td>
+            <td>${firstname.value}</td>
+            <td>${lastname.value}</td>
+            <td>${city.value}</td>
+            <td>${postalCode.value}</td>
+            <td>${phoneNumber.value}</td>
+            <td>${position.value}</td>
+            
+                           
+            `;
+
+            {/* <button class="edit">edit</button>
+        <button class="delete">delete</button></td> */}
+
+            userList.appendChild(row)
+            selectedRow = null
+            showAlert("student add", "danger")
+        }
+        else {
+            let update = userData.find(element => element.uid == uid.value)
+
+            update.firstname = firstname.value
+            update.lastname = lastname.value;
+            update.city = city.value;
+            update.postalCode = postalCode.value;
+            update.phoneNumber = phoneNumber.value;
+            update.position = position.value
+
+            tableBodyGenerator(userData)
+
+            selectedRow = null;
+            showAlert("student edited")
+        }
+
+        clearFields();
+    }
+
+})
+
+
+//edit
+edit.addEventListener("click", function (e) {
+    target = e.target
+
+    selectedRow = target.parentElement;
+
+    firstname.disabled = false
+    lastname.disabled = false
+    city.disabled = false
+    postalCode.disabled = false
+    phoneNumber.disabled = false
+    position.disabled = false
+    submit.style.display = 'block'
+
+
+})
+
+
+
+//delete
+deleteBtn.addEventListener("click", function (e) {
+
+    let deleteUser = userData.filter(element => element.uid != (uid.value));
+
+    tableBodyGenerator(deleteUser)
+
+    selectedRow = null;
+
+
+
+})
+
+//read
+userList.addEventListener("click", function (e) {
+    target = e.target
+
+    selectedRow = target.parentElement
+    uid.value = selectedRow.children[0].textContent
+    firstname.value = selectedRow.children[1].textContent
+    lastname.value = selectedRow.children[2].textContent
+    city.value = selectedRow.children[3].textContent
+    postalCode.value = selectedRow.children[4].textContent
+    position.value = selectedRow.children[5].textContent
+
+    uid.disabled = true
+    firstname.disabled = true
+    lastname.disabled = true
+    city.disabled = true
+    postalCode.disabled = true
+    phoneNumber.disabled = true
+    position.disabled = true
+    submit.style.display = 'none'
+})
